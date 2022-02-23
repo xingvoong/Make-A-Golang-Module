34793dbdbf164ab2bebe3c119673deeafd686d10
Xing Voong <xingvoong@gmail.com>
2022-02-23 12:20:52 -0800

    build path

--- sentences/sentences.go
+++ sentences/sentences.go
@@ -9,12 +9,11 @@ import (
 
 // Sentence returns a recommendation sentence for the named book.
 func Sentence(book_name string) (string, error) {
-	// If no book name was given, return an error with messsage of empty name
+
 	if book_name == "" {
 		return "", errors.New("empty name")
 	}
 
-	// return a recommendation sentence embeds the name of a book in a message
 	message := fmt.Sprintf(randomFormat(), book_name)
 	return message, nil
 }

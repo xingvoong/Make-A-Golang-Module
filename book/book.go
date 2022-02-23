38a11b5839ee924e44c6a374282acaedcc46349d
Xing Voong <xingvoong@gmail.com>
2022-02-23 10:52:27 -0800

    write error handling

--- book/book.go
+++ book/book.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/sentences"
 )
 
 func main() {
 	// Get a recommendation sentence and print it
-	message := sentences.Sentence("Ultralearning")
+	log.SetPrefix("sentences: ")
+	log.SetFlags(0)
+
+	// Request a recommender message
+	message, err := sentences.Sentence("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
--- sentences/sentences.go
+++ sentences/sentences.go
@@ -1,10 +1,18 @@
 package sentences
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Sentence returns a recommendation sentence for the named book.
-func Sentence(book_name string) string {
+func Sentence(book_name string) (string, error) {
+	// If no book name was given, return an error with messsage of empty name
+	if book_name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// return a recommendation sentence embeds the name of a book in a message
 	message := fmt.Sprintf("You would like to read %v", book_name)
-	return message
+	return message, nil
 }
